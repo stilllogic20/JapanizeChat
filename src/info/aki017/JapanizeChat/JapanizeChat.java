980974df48eef07438c33463f57cb8c8d61b9b93
azure_crystal_ <stillcrystal@outlook.jp>
2017-01-19 11:55:48 +0900

    [Change] Changed Logger (getLogger("Minecraft")→Bukkit#getLogger())

--- src/info/aki017/JapanizeChat/JapanizeChat.java
+++ src/info/aki017/JapanizeChat/JapanizeChat.java
@@ -1,5 +1,6 @@
 package info.aki017.JapanizeChat;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -7,7 +8,7 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 public class JapanizeChat extends JavaPlugin {
-  public static final Logger log = Logger.getLogger("Minecraft");
+  public static final Logger log = Bukkit.getLogger();
   private ConfigHandler configHandler;
 
   public void onDisable() {
@@ -18,23 +19,27 @@ public class JapanizeChat extends JavaPlugin {
   public void onEnable() {
     new JapanizeChatPlayerListener(this);
 
-    loadConfig();
+    try {
+      loadConfig();
+    } catch (IOException excepted) {
 
-    //Register commands
+    }
+
+    // Register commands
     getCommand("japanize").setExecutor(new JapanizeChatCommand(this));
 
-    //Print that the plugin has been enabled!
+    // Print that the plugin has been enabled!
     log.info("JapanizeChat version " + getDescription().getVersion() + " is enabled!");
 
   }
 
-  public void loadConfig() {
+  public void loadConfig() throws IOException {
     File configFile = new File(getDataFolder(), "config.yml");
 
     try {
       configFile.createNewFile();
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      throw exception;
     }
 
     configHandler = new ConfigHandler(configFile);
